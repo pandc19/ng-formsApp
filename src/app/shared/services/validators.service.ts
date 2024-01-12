a1825c513999ee9aa9ed7caffc856a017854a5aa
Jonathan Pillajo <pandc19@outlook.com>
2024-01-11 22:00:43 -0500

    fin sección 18

---
+++ src/app/shared/services/validators.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class ValidatorsService {
+
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+
+  public cantBeStrider = (control: FormControl): ValidationErrors | null => {
+
+    const value: string = control.value.trim().toLowerCase();
+
+    if (value === 'strider') {
+      return {
+        noStrider: true,
+      }
+    }
+
+    return null;
+  }
+
+  public isValidField(form: FormGroup, field: string) {
+    return form.controls[field].errors && form.controls[field].touched;
+  }
+
+  public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      if (field1Value !== field2Value) {
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true }
+      }
+
+      formGroup.get(field2)?.setErrors(null);
+      return null;
+    }
+  }
+
+}
---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,42 @@
+
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class EmailValidator implements AsyncValidator {
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   );
+  // }
+
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    const email = control.value;
+
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+
+      console.log({ email });
+      if (email === 'mail@mail.com') {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        // return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+    }).pipe(
+      delay(3000),
+    );
+
+    return httpCallObservable;
+  }
+
+}
